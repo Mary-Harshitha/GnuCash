0119c3b145404197563692679bde8b0a3b45eaa2
Mary-Harshitha <62737530+Mary-Harshitha@users.noreply.github.com>
2020-04-01 17:44:43 +0530

    user, removed from the file

--- lib/registration.dart
+++ lib/registration.dart
@@ -19,6 +19,7 @@ class _RegisterState extends State<Register>{
     if(formState.validate()){
       formState.save();
       try{
+
         await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _email, password: _password);
         Navigator.push(context,MaterialPageRoute(builder: (context) => Splash()));
         _emailController.clear();
