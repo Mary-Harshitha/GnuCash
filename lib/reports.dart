39f2ca4299efc0a03990d6c13d19e5a38e81ff08
Mary-Harshitha <62737530+Mary-Harshitha@users.noreply.github.com>
2020-04-08 14:47:38 +0530

    final reports

--- lib/reports.dart
+++ lib/reports.dart
@@ -1,50 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
-import 'package:flutter/animation.dart';
-import 'package:simple_animations/simple_animations.dart';
+//import 'package:flutter/animation.dart';
+//import 'package:simple_animations/simple_animations.dart';
+import 'pieChart.dart';
+import 'barChart.dart';
+import 'line.dart';
+import 'lineChart.dart';
 
 class Reports extends StatefulWidget{
-  _ReportState createState() => _ReportState();
+  final String userAddress;
+
+  Reports(this.userAddress);
+
+  _ReportState createState() => _ReportState(userAddress);
 }
 
 class _ReportState extends State<Reports>{
+  String userAddress;
 
-  Widget _buildBody(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
+  _ReportState(this.userAddress);
 
-      stream: Firestore.instance.collection('Accounts').snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData)
-          return LinearProgressIndicator();
-        else if (snapshot.hasError)
-          print('DATABASE ERROR: ${snapshot.error.toString()}');
-        return _buildList(context, snapshot.data.documents);
-      },
-    );
-  }
-
-  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-    return ListView(
-      scrollDirection: Axis.horizontal,
-      padding: const EdgeInsets.only(top: 3.0),
-      children: snapshot.map(
-              (data) => _buildListItem(context, data)
-      ).toList(),  //executes like a foreach statement
-    );
-  }
-
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-    final record = Record.fromSnapshot(data);
-
-    return Padding(
-        key: ValueKey(record.account),
-        padding: EdgeInsets.symmetric(vertical: 2,horizontal: 0),
-        child: Container(
-    height: 90,
-    child: BarChartApplication(record.account.toString(), record.money),
-    ));
-  }
+//  Widget _buildBody(BuildContext context) {
+//    return StreamBuilder<QuerySnapshot>(
+//
+//      stream: Firestore.instance.collection(userAddress).snapshots(),
+//      builder: (context, snapshot) {
+//        if (!snapshot.hasData)
+//          return LinearProgressIndicator();
+//        else if (snapshot.hasError)
+//          print('DATABASE ERROR: ${snapshot.error.toString()}');
+//        return _buildList(context, snapshot.data.documents);
+//      },
+//    );
+//  }
+//
+//  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+//    return ListView(
+//      scrollDirection: Axis.horizontal,
+//      padding: const EdgeInsets.only(top: 3.0),
+//      children: snapshot.map(
+//              (data) => _buildListItem(context, data)
+//      ).toList(),  //executes like a foreach statement
+//    );
+//  }
+//
+//  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+//    final record = Record.fromSnapshot(data);
+//
+//    return Padding(
+//        key: ValueKey(record.account),
+//        padding: EdgeInsets.symmetric(vertical: 2,horizontal: 0),
+//        child: Container(
+//    height: 90,
+//    child: BarChartApplication(record.account.toString(), record.money),
+//    ));
+//  }
 
   @override
   Widget build(BuildContext context) {
@@ -79,9 +90,14 @@ class _ReportState extends State<Reports>{
               children: <Widget>[
                 ButtonBar(buttonMinWidth: 190.0,
                   alignment: MainAxisAlignment.start,
-                  buttonHeight: 40,
+                  buttonHeight: 50,
                   children: <Widget>[
-                    FlatButton(onPressed: (){},
+                    FlatButton(onPressed: (){
+
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) =>
+                              PieChart(userAddress),fullscreenDialog: true));
+                    },
                       child: Row(
                         children: <Widget>[
                           Icon(Icons.pie_chart),
@@ -89,8 +105,16 @@ class _ReportState extends State<Reports>{
                           Text('PIE CHART')
                         ],
                       ),
-                      color: Colors.green,),
-                    FlatButton(onPressed: (){},
+                      color: Colors.purple,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)
+                        )),
+
+                    FlatButton(onPressed: (){
+
+                    Navigator.push(context,MaterialPageRoute(builder: (context) =>
+                          BarChart(userAddress),fullscreenDialog: true));
+                    },
                       child: Row(
                         children: <Widget>[
                           Icon(Icons.equalizer),
@@ -98,51 +122,62 @@ class _ReportState extends State<Reports>{
                           Text('BAR CHART')
                         ],
                       ),
-                      color: Colors.deepOrange,),
+                      color: Colors.pink,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)
+                        )),
                   ],
                 ),
-                ButtonBar(buttonMinWidth: 190.0,
-                  alignment: MainAxisAlignment.start,
-                  buttonHeight: 40,
-                  children: <Widget>[
-                    FlatButton(onPressed: (){},
-                      child: Row(
-                        children: <Widget>[
-                          Icon(Icons.trending_up),
-                          SizedBox(width: 10,),
-                          Text('LINE CHART')
-                        ],
-                      ),
-                      color: Colors.blue,),
-                    FlatButton(onPressed: (){},
-                      child: Row(
-                        children: <Widget>[
-                          Icon(Icons.subject),
-                          SizedBox(width: 10,),
-                          Text('SHEET')
-                        ],
-                      ),
-                      color: Colors.pink,),
-                  ],
-                ),
+//                ButtonBar(buttonMinWidth: 190.0,
+//                  alignment: MainAxisAlignment.start,
+//                  buttonHeight: 40,
+//                  children: <Widget>[
+//                    FlatButton(onPressed: (){
+//                      Navigator.push(context, MaterialPageRoute(builder: (context) =>
+//                          LineChart(userAddress),fullscreenDialog: true));
+//                    },
+//                      child: Row(
+//                        children: <Widget>[
+//                          Icon(Icons.trending_up),
+//                          SizedBox(width: 10,),
+//                          Text('LINE CHART')
+//                        ],
+//                      ),
+//                      color: Colors.blue,
+//                    shape: RoundedRectangleBorder(
+//                      borderRadius: BorderRadius.circular(20)
+//                    ),),
+//                    FlatButton(onPressed: (){
+//
+//                    },
+//                      child: Row(
+//                        children: <Widget>[
+//                          Icon(Icons.subject),
+//                          SizedBox(width: 10,),
+//                          Text('SHEET')
+//                        ],
+//                      ),
+//                      color: Colors.pink,
+//                        shape: RoundedRectangleBorder(
+//                            borderRadius: BorderRadius.circular(20)
+//                        )),
+//                  ],
+//                ),
+              SizedBox(height: 30,),
 
                 Container(
                     height: 300,
                     child:Padding(
-                        padding: EdgeInsets.all(10),
+                        padding: EdgeInsets.all(0),
                         child:Column(children: <Widget>[
                           Align(
-                            alignment: Alignment.bottomLeft,
-                            child:Text('All Transactions',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w300,fontSize: 18,
-                              ),),),
+                            alignment: Alignment.bottomLeft),
                           SizedBox(height: 10),
                           Expanded(
                           child:Card(
-                            color: Colors.black,
+//                            color: Colors.white,
                             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                            child: _buildBody(context),
+                            child: LineChartSample1(),
                           ))
                         ],)
                     )
@@ -156,97 +191,97 @@ class _ReportState extends State<Reports>{
   }
 }
 
-class Bar extends StatelessWidget {
-  final double height;
-  final String label;
-  final String amount;
-
-  final int _baseDurationMs = 1000;
-  final double _maxElementHeight = 100;
-
-  Bar(this.height, this.label,this.amount);
-
-  @override
-  Widget build(BuildContext context) {
-    return ControlledAnimation(
-      duration: Duration(milliseconds: (height * _baseDurationMs).round()),
-      tween: Tween(begin: 0.0, end: height),
-      builder: (context, animatedHeight) {
-        return Column(
-          children: <Widget>[
-            SizedBox(height: 50),
-            Container(
-              height: (1 - animatedHeight) * _maxElementHeight,
-            ),
-            Text('$amount',style: TextStyle(
-              fontSize: 12,color: Colors.yellow
-            ),),
-            Container(
-              width: 30,
-              height: animatedHeight * _maxElementHeight,
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topRight,
-                      end: Alignment.bottomLeft,
-                      stops: [0.1,0.2, 0.5, 0.6, 0.9],
-                      colors: [
-                        Colors.purple,
-                        Colors.purple,
-                        Colors.indigo,
-                        Colors.indigo,
-                        Colors.pink
-                      ])
-              ),
-            ),
-            SizedBox(height: 5,),
-            Text(label,style: TextStyle(
-                fontSize: 16,color: Colors.white
-            ),)
-          ],
-        );
-      },
-    );
-  }
-}
-
-class BarChartApplication extends StatelessWidget {
-  final String account;
-  final double money;
-  final double hh = 100.0;
-  final double tt = 1000.0;
-  BarChartApplication(this.account, this.money);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Bar((money)>100?(money)/tt:(money)/hh, account,money.toString()),
-        ],
-      ),
-    );
-  }
-}
-
-class Record {
-  String account;
-  String accountType;
-  double money;
-  bool favourite;
-  final DocumentReference reference;
-
-  Record.fromMap(Map<String, dynamic> map, {this.reference})
-      : assert(map['account_type'] != null), //make sure data isn't null, assert is for that
-        assert(map['account'] != null),
-        assert(map['money'] != null),
-        assert(map['fav'] != null),
-        account = map['account'],
-        accountType = map['account_type'],
-        favourite = map['fav'],
-        money = map['money'];
-
-  Record.fromSnapshot(DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data, reference: snapshot.reference);
-}
+//class Bar extends StatelessWidget {
+//  final double height;
+//  final String label;
+//  final String amount;
+//
+//  final int _baseDurationMs = 1000;
+//  final double _maxElementHeight = 100;
+//
+//  Bar(this.height, this.label,this.amount);
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return ControlledAnimation(
+//      duration: Duration(milliseconds: (height * _baseDurationMs).round()),
+//      tween: Tween(begin: 0.0, end: height),
+//      builder: (context, animatedHeight) {
+//        return Column(
+//          children: <Widget>[
+//            SizedBox(height: 50),
+//            Container(
+//              height: (1 - animatedHeight) * _maxElementHeight,
+//            ),
+//            Text('$amount',style: TextStyle(
+//              fontSize: 12,color: Colors.indigo
+//            ),),
+//            Container(
+//              width: 20,
+//              height: animatedHeight * _maxElementHeight,
+//              decoration: BoxDecoration(
+//                  gradient: LinearGradient(
+//                      begin: Alignment.topRight,
+//                      end: Alignment.bottomLeft,
+//                      stops: [0.1,0.2, 0.5, 0.6, 0.9],
+//                      colors: [
+//                        Colors.blue,
+//                        Colors.blue,
+//                        Colors.blue,
+//                        Colors.blue,
+//                        Colors.blue
+//                      ])
+//              ),
+//            ),
+//            SizedBox(height: 5,),
+//            Text(label,style: TextStyle(
+//                fontSize: 16,color: Colors.black
+//            ),)
+//          ],
+//        );
+//      },
+//    );
+//  }
+//}
+//
+//class BarChartApplication extends StatelessWidget {
+//  final String account;
+//  final double money;
+//  final double hh = 100.0;
+//  final double tt = 1000.0;
+//  BarChartApplication(this.account, this.money);
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Padding(
+//      padding: const EdgeInsets.all(20.0),
+//      child: Row(
+//        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//        children: <Widget>[
+//          Bar((money)>100?(money)/tt:(money)/hh, account,money.toString()),
+//        ],
+//      ),
+//    );
+//  }
+//}
+//
+//class Record {
+//  String account;
+//  String accountType;
+//  double money;
+//  bool favourite;
+//  final DocumentReference reference;
+//
+//  Record.fromMap(Map<String, dynamic> map, {this.reference})
+//      : assert(map['account_type'] != null), //make sure data isn't null, assert is for that
+//        assert(map['account'] != null),
+//        assert(map['money'] != null),
+//        assert(map['fav'] != null),
+//        account = map['account'],
+//        accountType = map['account_type'],
+//        favourite = map['fav'],
+//        money = map['money'];
+//
+//  Record.fromSnapshot(DocumentSnapshot snapshot)
+//      : this.fromMap(snapshot.data, reference: snapshot.reference);
+//}
